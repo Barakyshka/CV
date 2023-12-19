82fcaca7c20e554e2102e2eb22c530f81c3c7baa
Barakyshka <154346695+Barakyshka@users.noreply.github.com>
2023-12-20 01:37:52 +0300

    Update ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -4,19 +4,19 @@ function ProjectItem({ name, githubLink, description, technologies }) {
   return (
     <div className="bg-white p-4 rounded-md mb-4">
       <h3 className="text-xl font-bold mb-2">{name}</h3>
-      <div className="flex flex-col mb-2">
-        <span className="font-bold mb-1">GitHub:</span>
+      <article className="flex items-center mb-2">
+        <p className="font-bold">GitHub:</p>
         <a
           href={githubLink}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
+          className="text-blue-500 hover:underline ml-2"
         >
           {githubLink}
         </a>
-      </div>
-      <p className="text-sm mb-2">{description}</p>
-      <p className="text-sm">{technologies}</p>
+      </article>
+      <article className="text-sm mb-2">{description}</article>
+      <article className="text-sm">{technologies}</article>
     </div>
   );
 }
