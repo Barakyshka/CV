31e8ca5b34ec399e05a6dc5395fd6e14d0529c8b
Barakyshka <154346695+Barakyshka@users.noreply.github.com>
2023-12-20 01:59:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AuthorInfo from './components/AuthorInfo';
 function App() {
   return (
     
-    <div className="container mx-auto p-4">
+    <div class="container mx-auto p-2 md:p-4 lg:p-8">
       <AuthorInfo />
     </div>
   );
