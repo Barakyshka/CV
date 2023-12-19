c363f1e684f8c5834e96a3254c3139a0ad243e81
Barakyshka <154346695+Barakyshka@users.noreply.github.com>
2023-12-20 01:39:00 +0300

    Update PersonalInfo.jsx

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 function PersonalInfo() {
   return (
-    <div className="w-full mt-8 mx-auto flex flex-col gap-4 rounded-md p-4 md:w-1/2 lg:w-1/3">
+    <div className="w-full md:w-1/3 mt-8 md:mt-0 mx-auto flex flex-col gap-4 rounded-md">
       <div className="flex items-center">
         <img
           src="/images/geo.png"
@@ -18,47 +16,43 @@ function PersonalInfo() {
         </span>{' '}
         <br /> B1
       </div>
-      <div className="mb-2">
+      <ul className="list-disc ml-4">
         <span className="font-bold text-lg mb-2">Skills:</span>
-        <ul className="list-disc ml-4">
-          <li>C++</li>
-          <li>JS/React</li>
-          <li>C#</li>
-        </ul>
-      </div>
-      <div className="mb-2">
+        <li>C++</li>
+        <li>JS/React</li>
+        <li>C#</li>
+      </ul>
+      <ul className="list-disc ml-4">
         <span className="font-bold text-lg mb-2">Social Links:</span>
-        <ul className="list-disc ml-4">
-          <li>
-            <a href="mailto:ibarakyshkai@gmail.com" className="text-blue-500">
-              <img
-                src="/images/em.png"
-                alt="Telegram icon"
-                className="inline-block align-text-bottom"
-                style={{ height: '1.2em', width: '1.2em' }}
-              />
-              <span className="font-bold"> varakyshka@gmail.com </span>
-            </a>
-          </li>
-          <li>
-            <a href="https://t.me/barakyshka" className="text-blue-500">
-              <img
-                src="/images/tg.png"
-                alt="Telegram icon"
-                className="inline-block align-text-bottom"
-                style={{ height: '1.2em', width: '1.2em' }}
-              />
-              <span className="font-bold"> @varakyshka </span>
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li>
+          <a href="mailto:ibarakyshkai@gmail.com" className="text-blue-500">
+            <img
+              src="/images/em.png"
+              alt="Telegram icon"
+              className="inline-block align-text-bottom"
+              style={{ height: '1.2em', width: '1.2em' }}
+            />
+            <span className="font-bold"> varakyshka@gmail.com </span>
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/barakyshka" className="text-blue-500">
+            <img
+              src="/images/tg.png"
+              alt="Telegram icon"
+              className="inline-block align-text-bottom"
+              style={{ height: '1.2em', width: '1.2em' }}
+            />
+            <span className="font-bold"> @varakyshka </span>
+          </a>
+        </li>
+      </ul>
       <div className="mb-4">
         <a
           href="/resume.pdf"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded inline-block"
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
         >
           Распечатать PDF
         </a>
