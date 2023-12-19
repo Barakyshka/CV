344650a0db8d4a679a63326b2aacf5fb4de83dcd
Barakyshka <154346695+Barakyshka@users.noreply.github.com>
2023-12-20 01:30:19 +0300

    Update ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 function ProjectItem({ name, githubLink, description, technologies }) {
   return (
@@ -11,6 +11,30 @@ function ProjectItem({ name, githubLink, description, technologies }) {
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500 hover:underline ml-2"
+        >
+          {githubLink}
+        </a>
+      </article>
+      <article className="text-sm mb-2">{description}</article>
+      <article className="text-sm">{technologies}</article>
+    </div>
+  );
+}
+
+export default React.memo(ProjectItem);*/
+import React from 'react';
+
+function ProjectItem({ name, githubLink, description, technologies }) {
+  return (
+    <div className="bg-white p-4 rounded-md mb-4">
+      <h3 className="text-xl font-bold mb-2">{name}</h3>
+      <article className="flex flex-col md:flex-row md:items-center md:justify-between mb-2">
+        <p className="font-bold mb-2 md:mb-0">GitHub:</p>
+        <a
+          href={githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline md:max-w-xs truncate md:ml-2"
         >
           {githubLink}
         </a>
