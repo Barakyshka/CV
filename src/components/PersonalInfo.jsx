afe9d778ba46339b1c2b139d8ac3dc928ef4ebe9
Barakyshka <154346695+Barakyshka@users.noreply.github.com>
2023-12-20 01:31:56 +0300

    Update PersonalInfo.jsx

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 function PersonalInfo() {
   return (
@@ -63,4 +63,96 @@ function PersonalInfo() {
   );
 }
 
-export default React.memo(PersonalInfo);
+export default React.memo(PersonalInfo);*/
+import React from 'react';
+
+function ProjectItem({ name, githubLink, description, technologies }) {
+  return (
+    <div className="bg-white p-4 rounded-md mb-4">
+      <h3 className="text-xl font-bold mb-2">{name}</h3>
+      <div className="flex flex-col mb-2">
+        <span className="font-bold mb-1">GitHub:</span>
+        <a
+          href={githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          {githubLink}
+        </a>
+      </div>
+      <p className="text-sm mb-2">{description}</p>
+      <p className="text-sm">{technologies}</p>
+    </div>
+  );
+}
+
+function PersonalInfo() {
+  return (
+    <div className="w-full md:w-1/2 lg:w-1/3 mt-8 md:mt-0 mx-auto flex flex-col gap-4 rounded-md">
+      <div className="flex items-center">
+        <img
+          src="/images/geo.png"
+          alt="Telegram icon"
+          className="inline-block align-text-bottom"
+          style={{ height: '1.5em', width: '1.5em' }}
+        />
+        <span className="font-bold text-lg ml-2"> Minsk, Belarus </span>
+      </div>
+      <div className="">
+        <span className="font-bold text-lg mb-2">
+          English Language Proficiency
+        </span>{' '}
+        <br /> B1
+      </div>
+      <div className="mb-2">
+        <span className="font-bold text-lg mb-2">Skills:</span>
+        <ul className="list-disc ml-4">
+          <li>C++</li>
+          <li>JS/React</li>
+          <li>C#</li>
+        </ul>
+      </div>
+      <div className="mb-2">
+        <span className="font-bold text-lg mb-2">Social Links:</span>
+        <ul className="list-disc ml-4">
+          <li>
+            <a href="mailto:ibarakyshkai@gmail.com" className="text-blue-500">
+              <img
+                src="/images/em.png"
+                alt="Telegram icon"
+                className="inline-block align-text-bottom"
+                style={{ height: '1.2em', width: '1.2em' }}
+              />
+              <span className="font-bold"> varakyshka@gmail.com </span>
+            </a>
+          </li>
+          <li>
+            <a href="https://t.me/barakyshka" className="text-blue-500">
+              <img
+                src="/images/tg.png"
+                alt="Telegram icon"
+                className="inline-block align-text-bottom"
+                style={{ height: '1.2em', width: '1.2em' }}
+              />
+              <span className="font-bold"> @varakyshka </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="mb-4">
+        <a
+          href="/resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded inline-block"
+        >
+          Распечатать PDF
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export { ProjectItem, PersonalInfo };
+
